947dad244266c25d5b84f129f33ff8d60383ff35
AlbertBX <avh1972@mail.ru>
2024-11-29 00:39:10 +0500

    Delete lesson_3_task_3.py

--- lesson_3_task_3.py
+++
@@ -1,9 +0,0 @@
-from address import Address
-from mailing import Mailing
-
-to_address= Address(451254,"г.Москва", "ул.Лесная", 17,4)
-from_address=Address(478906,"г.Казань","ул.Авиаторов",31,79)
-
-mailing = Mailing (to_address, from_address, 747, 45668878798)
-
-print (mailing)
